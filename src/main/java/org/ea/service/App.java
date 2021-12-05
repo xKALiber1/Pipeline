80e5c05b22fea6b00990e580b323d480d1b9abc1
Brian <Brian.james.gomes@gmail.com>
2021-12-05 13:51:19 -0600

    s

--- src/main/java/org/ea/service/App.java
+++ src/main/java/org/ea/service/App.java
@@ -1,13 +1,31 @@
 package org.ea.service;
 
-/**
- * Hello world!
- *
- */
+import java.lang.exception
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.print( "Ahhh" );
+        sleep(400);
+        System.out.print( "Ahhh" );
+        sleep(400);
+        System.out.print( "Ahhh" );
+        sleep(400);
+        System.out.print( "Ahhh" );
+        sleep(400);
+        System.out.println( "Staying Alive!!" );
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        sleep(1000);
+    }
+    public static void sleep(int nanos) {
+        try {
+            Thread.sleep(nanos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
